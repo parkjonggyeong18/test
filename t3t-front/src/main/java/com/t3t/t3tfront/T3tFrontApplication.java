f907bbb4dd567ca09f0a1e16925bc9de85c6ea3a
parkjonggyeong18 <81209336+parkjonggyeong18@users.noreply.github.com>
2024-03-14 15:15:32 +0900

    Add files via upload

---
+++ t3t-front/src/main/java/com/t3t/t3tfront/T3tFrontApplication.java
@@ -0,0 +1,13 @@
+package com.t3t.t3tfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T3tFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T3tFrontApplication.class, args);
+	}
+
+}
---
+++ t3t-front/src/test/java/com/t3t/t3tfront/T3tFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t3t.t3tfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T3tFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
